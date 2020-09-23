ef8a6840af64dc698ef7619cd8f704f983a610d7
colour10 <84299915@qq.com>
2020-09-23 08:02:25 +0000

    邮箱验证

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 class PagesController extends Controller
 {
+    /**
+     * 首页
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function root()
     {
         return view('pages.root');
